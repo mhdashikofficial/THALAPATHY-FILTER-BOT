a17f0563b4658f23d72afc41d49535d0942fd227
rushikeshnarule <75885428+rushikeshnarule@users.noreply.github.com>
2023-09-06 00:43:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -96,8 +96,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://thalapathy-s8ws.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://thalapathy-s8ws.onrender.com/".format(FQDN, PORT)
+URL = "https://movie-search-bot-wdjx.onrender.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://movie-search-bot-wdjx.onrender.com".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
