1ef3da4384f446aaa0e2f1b890e28106e0980818
Muhammed Ashik <116175002+mhdashikofficial@users.noreply.github.com>
2024-04-23 11:37:55 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -16,7 +16,7 @@ class script(object):
 Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.</b>"""
 
     ABOUT_TXT = """<b>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟
-‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/Tamilmoviesrio_bot>CAT WOMEN</a>
+‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/catwomen_movie_bot>CAT WOMEN</a>
  ‣ My best friend : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
  ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/VIRUSEPIC'>KAKASHI</a> 
  ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
